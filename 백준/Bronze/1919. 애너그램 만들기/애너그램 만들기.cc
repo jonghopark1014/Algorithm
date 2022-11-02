ae6f571bcd932920d02f4fb7f77daf8ee23afa8a
PARK JONG HO <109330533+jonghopark1014@users.noreply.github.com>
2022-11-02 14:34:11 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.cc
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.cc
@@ -1,47 +1,29 @@
 #include <iostream>
-#include <bits/stdc++.h>
-
 using namespace std;
 
+int arr[26];
 
 int main() {
-    ios_base :: sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    char str1[1002], str2[1002];
+    cin >> str1;
+    cin >> str2;
+    int i = 0, j = 0, result = 0;
+    while (str1[i]) {
+        arr[(int)str1[i] - 'a'] += 1;
+        i += 1;
+    }
+    while (str2[j]) {
+        arr[(int)str2[j] - 'a'] -= 1;
+        j += 1;
+    }
+    for (int k = 0; k < 26; ++k) {
+        if (arr[k] > 0) {
+            result += arr[k];
+        } else if (arr[k] < 0) {
+            result -= arr[k];
+        }
+    }
 
-    string alphabet;
-    alphabet = "abcdefghijklmnopqrstuvwxyz";
-    int alphaA[26] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int alphaB[26] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    string A, B;
-    cin >> A;
-    cin >> B;
-    for (int k = 0; k < A.length(); k++) {
-        for (int j = 0; j < 26; j++) {
-            if (alphabet[j] == A[k]) {
-                alphaA[j] += 1;
-            }
-        }
-    }
-    for (int l = 0; l < B.length(); l++) {
-        for (int j = 0; j < 26; j++) {
-            if (alphabet[j] == B[l]) {
-                alphaB[j] += 1;
-            }
-        }
-    }
-    int ans = 0;
-    for (int m = 0; m < 26; m++) {
-        if (alphaA[m] != alphaB[m]) {
-            if (alphaA[m] - alphaB[m] < 0) {
-                ans += (alphaA[m] - alphaB[m]) * -1;
-            }
-            else {
-                ans += alphaA[m] - alphaB[m];
-            }
-        }
-    }
-    cout << ans << endl;
-
+    cout << result << endl;
     return 0;
 }

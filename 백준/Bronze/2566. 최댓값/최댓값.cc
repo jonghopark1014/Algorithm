6fc0379d3ca08f694c811236aea7a664c4b5aa89
PARK JONG HO <109330533+jonghopark1014@users.noreply.github.com>
2022-11-07 13:20:19 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.cc
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -1,26 +1,25 @@
-#include<iostream>
-#include<bits/stdc++.h>
-
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios_base :: sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int maxR = 0, maxC = 0, maxV = 0;
-    for (int r = 0; r < 9; r++) {
-        for (int c = 0; c < 9; c++) {
-            int Num;
-            cin >> Num;
-            if (Num > maxV) {
-                maxV = Num;
-                maxR = r;
-                maxC = c;
-
-            }
+    int maxNum = 0, maxNumR = 0, maxNumC = 0, num, R = 0, C = -1;
+    for (int i = 0; i < 81; i++) {
+        cin >> num;
+        C += 1;
+        if (C > 8) {
+            C = 0;
+            R += 1;
+        }
+        if (num > maxNum) {
+            maxNum = num;
+            maxNumR = R;
+            maxNumC = C;
         }
     }
-    cout << maxV << endl;
-    cout << maxR + 1 << ' ' << maxC + 1;
+    cout << maxNum << '\n';
+    cout << maxNumR + 1 << ' ' << maxNumC + 1;
+
+
     return 0;
 }

38917d1106c0ea41bd2dd1a66a0e7a22a30ac697
PARK JONG HO <109330533+jonghopark1014@users.noreply.github.com>
2022-12-14 11:47:02 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,28 +1,100 @@
-#include<iostream>
-#include<bits/stdc++.h>
-
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int alphaNum[26] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-map<string, int> Alphabet;
+
+int arr[26] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 int main() {
-    string tmp = "abcdefghijklmnopqrstuvwxyz";
-    for (int i=0; i < 26; i++) {
-        string tmp2;
-        tmp2 = tmp[i];
-        Alphabet.insert({tmp2, i});
-    }
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     string word;
     cin >> word;
-    int wordLength = word.length();
-    for (int j = 0; j < wordLength; j++) {
-        string tmp3 = "";
-        tmp3 = word[j];
-        int tmp4 = Alphabet[tmp3];
-        alphaNum[tmp4] += 1;
+
+    for (int i = 0; i < word.length(); i++) {
+        if (word[i] == 'a') {
+            arr[0] += 1;
+        }
+        else if (word[i] == 'b') {
+            arr[1] += 1;
+        }
+        else if (word[i] == 'c') {
+            arr[2] += 1;
+        }
+        else if (word[i] == 'd') {
+            arr[3] += 1;
+        }
+        else if (word[i] == 'e') {
+            arr[4] += 1;
+        }
+        else if (word[i] == 'f') {
+            arr[5] += 1;
+        }
+        else if (word[i] == 'g') {
+            arr[6] += 1;
+        }
+        else if (word[i] == 'h') {
+            arr[7] += 1;
+        }
+        else if (word[i] == 'i') {
+            arr[8] += 1;
+        }
+        else if (word[i] == 'j') {
+            arr[9] += 1;
+        }
+        else if (word[i] == 'k') {
+            arr[10] += 1;
+        }
+        else if (word[i] == 'l') {
+            arr[11] += 1;
+        }
+        else if (word[i] == 'm') {
+            arr[12] += 1;
+        }
+        else if (word[i] == 'n') {
+            arr[13] += 1;
+        }
+        else if (word[i] == 'o') {
+            arr[14] += 1;
+        }
+        else if (word[i] == 'p') {
+            arr[15] += 1;
+        }
+        else if (word[i] == 'q') {
+            arr[16] += 1;
+        }
+        else if (word[i] == 'r') {
+            arr[17] += 1;
+        }
+        else if (word[i] == 's') {
+            arr[18] += 1;
+        }
+        else if (word[i] == 't') {
+            arr[19] += 1;
+        }
+        else if (word[i] == 'u') {
+            arr[20] += 1;
+        }
+        else if (word[i] == 'v') {
+            arr[21] += 1;
+        }
+        else if (word[i] == 'w') {
+            arr[22] += 1;
+        }
+        else if (word[i] == 'x') {
+            arr[23] += 1;
+        }
+        else if (word[i] == 'y') {
+            arr[24] += 1;
+        }
+        else if (word[i] == 'z') {
+            arr[25] += 1;
+        }
     }
-    for (int k = 0; k < 26; k++) {
-        cout << alphaNum[k] << ' ';
+    for (int i = 0; i < 26; i++) {
+        cout << arr[i] << ' ';
     }
+
     return 0;
 }
+

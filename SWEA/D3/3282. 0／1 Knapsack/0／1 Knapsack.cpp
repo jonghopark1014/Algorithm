8ace0005ed5676c0ccfc3546b3521ffffea8016a
PARK JONG HO <109330533+jonghopark1014@users.noreply.github.com>
2023-07-08 11:54:14 +0900

    [D3] Title: 0/1 Knapsack, Time: 17 ms, Memory: 13,940 KB -BaekjoonHub

---
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int T, N, K, V[101], C[101], arr[101][1001];
+
+void input() {
+    cin >> N >> K;
+    for (int r = 1; r < N + 1; r++) {
+        cin >> V[r] >> C[r];
+    }
+}
+
+int knapsack(int index, int vol) {
+    for (int r = 1; r < index + 1; r++) {
+        for (int c = 1; c < vol + 1; c++) {
+            if (V[r] <= c) {
+                arr[r][c] = max(arr[r - 1][c - V[r]] + C[r], arr[r-1][c]);
+            }
+            else {
+                arr[r][c] = arr[r-1][c];
+            }
+        }
+    }
+    return arr[index][vol];
+}
+
+int main() {
+    cin >> T;
+    for (int tc = 1; tc < T + 1; tc++) {
+        input();
+        cout << "#" << tc << ' ' << knapsack(N, K) << '\n';
+    }
+
+    return 0;
+}

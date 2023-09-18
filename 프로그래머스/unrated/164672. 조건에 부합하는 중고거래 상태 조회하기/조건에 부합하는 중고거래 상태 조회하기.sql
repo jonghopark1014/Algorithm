b73b22477b6839280279df80aa93872a976f3b1f
PARK JONG HO <109330533+jonghopark1014@users.noreply.github.com>
2023-09-18 18:46:58 +0900

    [unrated] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,12 @@
+SELECT
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    IF(STATUS = 'DONE', '거래완료', IF(STATUS = 'SALE', '판매중', '예약중')) as "STATUS"
+FROM
+    USED_GOODS_BOARD
+WHERE
+    CREATED_DATE = '2022-10-5'
+ORDER BY
+    BOARD_ID DESC
